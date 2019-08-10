8ee4a0f3738460918086d8cec2147717c17a06d1
it-one-mm <itonemm@gmail.com>
2019-08-10 23:17:05 +0630

    handling events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,42 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
 
-    state = {
-        count: 0,
-        tags: []
-    }
+  handleIncrement() {
+    console.log("Increment Clicked!", this);
+  }
 
-    renderTags() {
-        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-        
-        return (
-            <ul>
-                {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-            </ul>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <span style={{ fontSize: 15 }} className={this.getBadgeClasses()}>
+          {this.formatCounter()}
+        </span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Incrememnt
+        </button>
+      </React.Fragment>
+    );
+  }
 
-    render() {
-        return (
-            <React.Fragment>
-                <span style={{ fontSize: 15 }} className={this.getBadgeClasses()}>{this.formatCounter()}</span>
-                <button className="btn btn-secondary btn-sm">Incrememnt</button>
-                {this.renderTags()}
-            </React.Fragment>
-        );
-    }
+  formatCounter = () => {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  };
 
-    formatCounter = () => {
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count;
-    }
-
-    getBadgeClasses() {
-        let classes = 'badge m-2 badge-';
-        classes += (this.state.count === 0) ? 'warning' : 'primary';
-        return classes;
-    }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 }
 
 export default Counter;
