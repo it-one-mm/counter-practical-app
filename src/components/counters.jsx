cab83a420bcd34b4a836af760e3d4af5679cb773
it-one-mm <itonemm@gmail.com>
2019-08-16 17:44:58 +0630

    Lifting State Up (Step 1)

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,47 +2,17 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  handleIncrement = counter => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  }
-
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({counters: counters});
-  }
-
-  handleDelete = counterId => {
-    // console.log('Event Handler called.', counterId);
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({counters: counters});
-  }
 
   render() {
     return (
       <React.Fragment>
-        <button onClick={this.handleReset} className="btn btn-primary">Reset</button>
-        {this.state.counters.map(counter => (
+        <button onClick={this.props.onReset} className="btn btn-primary">Reset</button>
+        {this.props.counters.map(counter => (
           <Counter 
             key={counter.id} 
             counter={counter}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement} />
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement} />
         ))}
       </React.Fragment>
     );
