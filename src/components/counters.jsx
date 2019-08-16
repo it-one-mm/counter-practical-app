3a966b0236c756ab6cabe314eddd5f27519ec094
it-one-mm <itonemm@gmail.com>
2019-08-16 16:32:32 +0630

    Single Source of Truth

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,6 +11,14 @@ class Counters extends Component {
     ]
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({counters: counters});
+  }
+
   handleDelete = counterId => {
     // console.log('Event Handler called.', counterId);
     const counters = this.state.counters.filter(c => c.id !== counterId);
@@ -20,6 +28,7 @@ class Counters extends Component {
   render() {
     return (
       <React.Fragment>
+        <button onClick={this.handleReset} className="btn btn-primary">Reset</button>
         {this.state.counters.map(counter => (
           <Counter onDelete={this.handleDelete} key={counter.id} counter={counter} />
         ))}
